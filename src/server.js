4e625d024daadc5e2303a798ac43f1ecef37adb3
loscalzom <marceloscalzo@gmail.com>
2025-01-30 20:35:38 -0300

    otro

--- src/server.js
+++ src/server.js
@@ -46,4 +46,4 @@ export default app
 /* app.listen(PORT, () => {    
     console.log(`El servidor se esta ejecutando en el puerto ${PORT}`)}) */
 /* 
-    sendMail({to: "marceloscalzo@gmail.com",subject: "mensaje de prueba", html:`<h1>Hola desde Node.js</h1>`}) */
+    sendMail({to: "marceloscalzo@gmail.com",subject: "mensaje de  prueba", html:`<h1>Hola desde Node.js</h1>`}) */
