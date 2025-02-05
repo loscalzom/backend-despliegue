fd9834fda3aa002e8f258e6f04cac012094a1c81
loscalzom <marceloscalzo@gmail.com>
2025-02-05 12:21:36 -0300

    sin save

--- src/repository/user.repository.js
+++ src/repository/user.repository.js
@@ -2,34 +2,7 @@ import pool from "../config/mysql.config.js";
 import User from "../models/User.model.js";
 
 class UserRepository{
-    /* 
-    MONGO_DB
-    async createUser ({username, email, password, verificationToken}){
-        const nuevo_usuario = new User({
-            username,
-            email, 
-            password,
-            verificationToken,
-            modifiedAt: null
-        })
-        return await nuevo_usuario.save()
-    } 
-
-    async findUserByEmail (email){
-        return await User.findOne({email: email})
-    }
-
-    async findById(id){
-        return await User.findById(id)
-    }
-
-    async verifyUser( user_id ){
-        const user = await this.findById(user_id)
-        user.verified = true
-        user.save()
-    }
-    */
-    //MYSQL
+   
     async createUser({username, email, password, verificationToken}){
         const queryStr =  `
         INSERT INTO USERS (username, email, password, verificationToken)
