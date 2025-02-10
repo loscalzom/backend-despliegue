6f17ca993decdf43a97063cb1aae4618245dc7ee
loscalzom <marceloscalzo@gmail.com>
2025-02-10 16:18:31 -0300

    pasar a Number userid

--- src/middlewares/isWorkspaceMember.middleware.js
+++ src/middlewares/isWorkspaceMember.middleware.js
@@ -2,8 +2,8 @@ import WorkspaceRepository from "../repository/workspaces.repository.js";
 
 const isWorkspaceMemberMiddleware = async (req, res, next) => {
     try {
-        const { id } = req.user;  // ID del usuario extraído del payload del token
-        const { workspace_id } = req.params;  // workspace_id extraído de los parámetros de la URL
+        const { id } = req.user;  // El id del usuario está en el payload del token o en el request
+        const { workspace_id } = req.params;  // El workspace_id está en los parámetros de la URL
 
         // Verificar si el workspace existe
         const workspace_selected = await WorkspaceRepository.findWorkspaceById(workspace_id);
@@ -14,6 +14,9 @@ const isWorkspaceMemberMiddleware = async (req, res, next) => {
             });
         }
 
+        // Convertimos el user_id de la base de datos (número) a string antes de comparar
+        const user_id_db = String(workspace_selected.owner); // Convertir a string
+
         // Verificar si el usuario es miembro del workspace
         const isMember = await WorkspaceRepository.isUserMemberOfWorkspace(id, workspace_id);
         if (!isMember) {
@@ -23,9 +26,17 @@ const isWorkspaceMemberMiddleware = async (req, res, next) => {
             });
         }
 
-        // Si todo está bien, se adjunta el workspace encontrado al objeto `req` y se llama a `next()`
+        // Comparar el user_id del token con el del workspace (ya convertido a string)
+        if (id !== user_id_db) {
+            return res.status(403).json({
+                ok: false,
+                message: 'You are not the owner of this workspace'
+            });
+        }
+
+        // Si todo está bien, pasamos al siguiente middleware o controlador
         req.workspace_selected = workspace_selected;
-        next();  // Llamamos al siguiente middleware o controlador
+        next();
     } catch (error) {
         console.error(error);
         return res.status(500).json({
