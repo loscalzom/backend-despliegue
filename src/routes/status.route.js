f01827e34d395698c49eae61184747564b30ab9e
loscalzom <marceloscalzo@gmail.com>
2025-02-12 16:29:47 -0300

    borrando importaciones no usadas

--- src/routes/status.route.js
+++ src/routes/status.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { authMiddleware } from '../middlewares/auth.middleware.js'
-import { middlewareDePrueba } from '../middlewares/luck.middleware.js'
+
 const statusRoute = express.Router()
 
 statusRoute.get('/ping', (request, response)=>{
